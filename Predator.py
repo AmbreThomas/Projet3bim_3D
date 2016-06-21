a890362735b06f43eca3a80a5ce1d0269acdb3af
DanielaCortes <daniela.cortes@insa-lyon.fr>
2016-06-21 09:40:19 +0200

    Predateur

---
+++ Predator.py
@@ -0,0 +1,10 @@
+import random
+
+class Predator :
+  
+  def __init__(self) :
+    self.x = random.randint(0,500)
+    self.y = random.randint(0,500)
+    self.z = random.randint(0,1200)
+    self.age = 0
+    
